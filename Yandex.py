372ab2535be1eafb07a40fe99c72e600f244c921
Barinov <barinovgm@gmail.com>
2021-07-26 22:39:38 +0300

    Add - func for create JSON. - comments for methods.

--- Yandex.py
+++ Yandex.py
@@ -17,10 +17,7 @@ class Yandex_user():
 
     # Создаём папку с нужным нам именем на Я.Диске
     def newfolder(self):
-        folderName = str(
-            input(
-                'Введите название папки для сохранения фотографий на Яндекс Диске: '
-            ))
+        folderName = str(input('Введите название папки для сохранения фотографий на Яндекс Диске: '))
         newfolder_url = self.uri + 'v1/disk/resources'
         headers = self.get_headers()
         params = {"path": folderName, "overwrite": "true"}
@@ -31,7 +28,6 @@ class Yandex_user():
 
     # Производим загрузку файлов на Я.Диск. В параметрах ([Словарь полученный из ВК, с данными для загрузки][Имя папки, в которую будут загружены фото])
     def upload_by_url(self, name_url_dict=dict, folderName=str):
-        data = {}
         upload_url = self.uri + 'v1/disk/resources/upload'
         headers = self.get_headers()
         for vk_name, vk_url in tqdm(name_url_dict.items()):
@@ -46,24 +42,22 @@ class Yandex_user():
         pprint(response.json())
         return response.json()
 
-    #Получим список загруженных фото на ЯД для составления JSON
+    #Получим список загруженных фото на ЯД для составления JSON. В параметрах папка, в которую загружали фото
     def get_information(self, path=str):
         get_information_url = self.uri + 'v1/disk/resources'
         headers = self.get_headers()
         params = {'path': path}
         response = requests.get(get_information_url, headers=headers, params=params)
-        # pprint(response.json())
         return response.json()['_embedded']['items']
 
-
-    # def create_json(self, photo_name, photo_size):
-    #     data = {}
-    #     for name in photo_name:
-    #         for type in photo_size:
-    #             data['file_name'] = [name]
-    #             data['size'] = [type]
-    #         with open("data_file.json", "a") as f:
-    #             json.dump(data, f)
-    #             print(
-    #                 f'Информация о загрузке {vk_name}.jpg добавлена в json'
-    #             )
+    #Создадим джейсон по имени и размеру загруженных файлов. Информацию о загруженных файлах получаем с Яндекса, и вытаскиваем от туда их имена.
+    #Информацию о размерах, обозначенных буквами, фото вытаскиваем из ВК
+    def create_json(self, photo_name, photo_size):
+        data = {}
+        for name in photo_name:
+            for size in photo_size:
+                data['file_name'] = [name['name']]
+                data['size'] = [size]
+            with open("data_file.json", "a") as f:
+                json.dump(data, f)
+                print(f"Информация о загрузке {name['name']} добавлена в json")
--- main.py
+++ main.py
@@ -1,17 +1,18 @@
 import VK
 import Yandex
 from pprint import pprint
-
 with open("data_file.json", "w") as f:
   pass
-
 with open('api_vk.txt', encoding='utf-8') as f:
     VKtoken = f.read()
 
 
-
+#Инициализация пользователей
 Im_YA = Yandex.Yandex_user()
 Im_VK = VK.VK_user(VKtoken, '5.131')
-# Im_YA.upload_by_url(Im_VK.get_url_photos(), Im_YA.newfolder())
 
-# pprint(Im_YA.get_information('777'))
+#Работа метедов
+#Загрузка фото из ВК в Яндекс
+Im_YA.upload_by_url(Im_VK.get_url_photos(), Im_YA.newfolder())
+#Создание JSON по загруженным файлам
+Im_YA.create_json(Im_YA.get_information(), Im_VK.get_size_photos())
